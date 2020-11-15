bc4d2b19f6e8a7c2100f70fe6f121cf64c2a2e58
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-11-15 13:18:11 +0530

    Successfully Executed!!

--- hello_world/popup.js
+++ hello_world/popup.js
@@ -1,5 +1,5 @@
 $(function() {
     $("#get_name").keyup(function() {
-        $("#name").text("Hello " + ("#get_name").value());
+        $("#name").text("Hello " + $("#get_name").val());
     });
 });
