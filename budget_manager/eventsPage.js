0150f1602b4ca67d1695b7452c8fe53b38d5b939
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-11-17 01:32:24 +0530

    Added the functionality for context menu.

--- budget_manager/eventsPage.js
+++ budget_manager/eventsPage.js
@@ -5,3 +5,34 @@ var contextMenuObject = {
 };
 
 chrome.contextMenus.create(contextMenuObject);
+
+function isInt(value) {
+    return !isNaN(value) && parseInt(value) == value && !isNaN(parseInt(value, 10));
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+    if (clickData.menuItemId == "SpendMoney" && clickData.selectionText) {
+        if (isInt(clickData.selectionText)) {
+            chrome.storage.sync.get(["limit", "total"], function(budget) {
+                var newTotal = 0;
+                if (budget.total) {
+                    newTotal += parseInt(budget.total);
+                }
+                newTotal += parseInt(clickData.selectionText);
+                chrome.storage.sync.set({ "total": newTotal }, function() {
+                    var notificationObject = {
+                        type: "basic",
+                        title: "Limit Exceeded!!",
+                        message: "Looks like you have exceeded your limit!!",
+                        iconUrl: "moneyicon.jpg"
+                    }
+                    if (parseInt(budget.limit) <= parseInt(budget.total)) {
+                        chrome.notifications.create(notificationObject, function() {
+                            console.log("Notified!!");
+                        });
+                    }
+                });
+            });
+        }
+    }
+});
